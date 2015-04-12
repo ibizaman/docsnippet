9a45f399c474f80420ff46d0de5aab35800593b7
Pierre Penninckx <ibizapeanut@gmail.com>
2015-04-12 15:47:55 +0200

    add script to install yaourt

---
+++ install-yaourt.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd_to="$1"
+[ -z "$cd_to" ] && cd_to=.
+
+set -e
+
+cd $cd_to
+
+curl -O https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz
+tar zxvf package-query.tar.gz
+cd package-query
+makepkg -si
+cd ..
+
+curl -O https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz
+tar zxvf yaourt.tar.gz
+cd yaourt
+makepkg -si
+cd ..
